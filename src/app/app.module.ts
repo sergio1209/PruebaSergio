2d862781bfdf401ba6a50f30ee3a0e03f31197eb
sergio Diaz <cheyoivan@gmail.com>
2021-05-30 18:54:53 -0500

    comunicacion de componentes
    comunicamos los componentes index y listahistorial
    
    creamos un botor guardar y un historial de operaciones con la posibilidad de editar esas operaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 import {FormsModule} from "@angular/forms";
+import { ListaHistorialComponent } from './lista-historial/lista-historial.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    IndexComponent
+    IndexComponent,
+    ListaHistorialComponent
   ],
   imports: [
     BrowserModule,
